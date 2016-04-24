eaec4126c27bb0f4ec0edc1add0e7f3e47c571ab
KoMantas <m.kontrimas@yahoo.com>
2016-04-24 19:45:57 +0300

    Graph structure completed

---
+++ Graph/Graph.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouterProtocol.Graph
+{
+    public class Graph
+    {
+        private List<Node> _nodes = new List<Node>();
+
+        public Graph(){}
+
+        public bool AddNode(Node newNode)
+        {
+            if (_nodes.Contains(newNode))
+            {
+                _nodes.Add(newNode);
+                return true;
+            }
+            return false;
+        }
+
+        public Node[] GetGraphNodes()
+        {
+            return _nodes.ToArray();
+        }
+    }
+}
---
+++ Graph/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouterProtocol.Graph
+{
+    public class Node
+    {
+        private static int numOfNodes = 0;
+        public Dictionary<int, Node> Vertexes { get; private set; }
+        public int ID{ get; private set; }
+        public string Info { get; private set; }
+
+        public Node(string info)
+        {
+            Info = info;
+            ID = numOfNodes++;
+            Vertexes = new Dictionary<int, Node>();
+        }
+
+
+        public bool AddNewVertex(Node newVertex)
+        {
+            if (Vertexes.ContainsKey(newVertex.ID))
+            {
+                Vertexes.Add(newVertex.ID, newVertex);
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveVertex(int removedVertexId)
+        {
+            if (Vertexes.ContainsKey(removedVertexId))
+            {
+                Vertexes.Remove(removedVertexId);
+                return true;
+            }
+            return false;
+        }
+    }
+}
